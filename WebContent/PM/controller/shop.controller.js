79632b0859e38d787a9204c9b8457c3d60d621ce
impnguyen <imoney91@me.com>
2017-02-07 23:03:01 +0100

    push

--- WebContent/PM/controller/shop.controller.js
+++ WebContent/PM/controller/shop.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/m/MessageToast",
-  "sap/ui/Device", 
+  "sap/ui/Device",
   "sap/ui/model/json/JSONModel"
 ], function (Controller, MessageToast, Device, JSONModel) {
   "use strict";
@@ -13,26 +13,44 @@ sap.ui.define([
       // set device model
       var oDeviceModel = new JSONModel(Device);
       this.getView().setModel(oDeviceModel, "device");
+
+      //init geschaefte entityset
+      this.getGeschaeftEntitySet();
     },
 
     onAfterRendering: function () {
 
-    }, 
+    },
 
-    onAddLocation: function(oEvent){
+    onAddLocation: function (oEvent) {
       this.getView().byId("addShopDialog").open();
-    }, 
+    },
 
-    onAddShop: function(oEvent){
+    onAddShop: function (oEvent) {
       this.getView().byId("addShopDialog").close();
-    }, 
+    },
 
-    onCancelShop: function(oEvent){
+    onCancelShop: function (oEvent) {
       this.getView().byId("addShopDialog").close();
-    }, 
+    },
 
-    onNavBack: function(){
+    onNavBack: function () {
       this.getView().oParent.oParent.backToTopMaster();
+    },
+
+    getGeschaeftEntitySet: function (callback) {
+      var oThat = this;
+
+      $.ajax("http://192.168.20.20:3000/GeschaeftEntitySet")
+        .done(function (data, textStatus, jqXHR) {
+          var oModel = new JSONModel();
+          oModel.setData(data);
+          oThat.getView().setModel(oModel, "Geschaefte");
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+          debugger
+        });
+
     }
 
   });
