8572758f113043cf8ff135fd742e3aeb81de3850
impnguyen <imoney91@me.com>
2017-06-24 00:07:32 +0200

    - added i18n properties for en and de - refactoring calendar view

--- WebContent/PM/controller/overview.controller.js
+++ WebContent/PM/controller/overview.controller.js
@@ -197,21 +197,21 @@ sap.ui.define([
 		  */
 		setPageTitle : function(iMonth) {
 			var month = [];
-			month[0] = "Januar";
-			month[1] = "Februar";
-			month[2] = "März";
+			month[0] = "January";
+			month[1] = "February";
+			month[2] = "March";
 			month[3] = "April";
-			month[4] = "Mai";
-			month[5] = "Juni";
-			month[6] = "Juli";
+			month[4] = "May";
+			month[5] = "June";
+			month[6] = "July";
 			month[7] = "August";
 			month[8] = "September";
-			month[9] = "Oktober";
+			month[9] = "October";
 			month[10] = "November";
-			month[11] = "Dezember";
+			month[11] = "December";
 			var sMonth = month[iMonth];
 
-			this.getView().byId("overviewPage").setTitle("Einkäufe: " + sMonth);
+			this.getView().byId("overviewPage").setTitle("Purchases: " + sMonth);
 		},
 
 		/**
