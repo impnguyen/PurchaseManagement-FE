d0cb70aee65f10eb512e6eb2da13647d94c7e8e3
impnguyen <imoney91@me.com>
2017-12-31 14:01:23 +0100

    refactored devicemodel for purchasecontroller; send auth jwt token to backend

--- WebContent/PM/controller/overview.controller.js
+++ WebContent/PM/controller/overview.controller.js
@@ -1,310 +1,354 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
+
 /**
  * @module Overview
  * @author Manh Phuoc Nguyen
  */
-sap.ui.define([
-	"mpn/PM/controller/BaseController",
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"sap/ui/Device",
-	"sap/ui/model/json/JSONModel",
-	"mpn/PM/js/formatter",
-	'sap/ui/unified/DateTypeRange',
-	"mpn/PM/model/Shop",
-	"mpn/PM/model/Purchase",
+sap.ui.define(
+  [
+    "mpn/PM/controller/BaseController",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "mpn/PM/js/formatter",
+    "sap/ui/unified/DateTypeRange",
+    "mpn/PM/model/Shop",
+    "mpn/PM/model/Purchase",
 	"mpn/PM/model/Payer"
-], function(BaseController, Controller, MessageToast, Device, JSONModel, formatter, DateTypeRange, Shop, Purchase, Payer) {
-	"use strict";
+  ],
+  function(
+    BaseController,
+    MessageToast,
+    JSONModel,
+    formatter,
+    DateTypeRange,
+    Shop,
+    Purchase,
+	Payer
+  ) {
+    "use strict";
+    return BaseController.extend("mpn.PM.controller.overview", {
+      formatter: formatter,
 
-	return BaseController.extend("mpn.PM.controller.overview", {
-		formatter : formatter,
-
-		/**
-		 * set device model and triggers overview setup
+      /**
+		 * set device model 
 		 * @memberOf module:Overview
 		 */
-		onInit : function() {
-			this.setupModels();
-		},
+      onInit: function() {
+        this.setupDeviceModel();
+      },
 
-		/**
+      /**
 		 * triggers overview setup
 		 * @memberOf module:Overview
 		 */
-		onAfterRendering : function() {
-			this.setupOverview();
-		},
+      onAfterRendering: function() {
+        this.setupOverview();
+      },
 
-		/**
+      /**
 		 * navigate back to top master view
 		 * @memberOf module:Overview
 		 */
-		onNavBack : function() {
-			this.getView().oParent.oParent.backToTopMaster();
-		},
+      onNavBack: function() {
+        this.getView().oParent.oParent.backToTopMaster();
+      },
 
-		/**
+      /**
 		 * open calendar popover
 		 * @memberOf module:Overview
 		 * @param {object} oEvent - item press event
 		 */
-		onCal : function(oEvent) {
-			this.getView().byId("calendarPopover").openBy(oEvent.getSource());
-		},
+      onCal: function(oEvent) {
+        this.getView().byId("calendarPopover").openBy(oEvent.getSource());
+      },
 
-
-		/**
+      /**
 		 * setup overview models and binding
 		 * @memberOf module:Overview
 		 */
-		setupOverview : function() {
-			//deferred objects
-			var oDefEinkauf = jQuery.Deferred();
-			var oDefZahler = jQuery.Deferred();
-			var oDefGeschaeft = jQuery.Deferred();
+      setupOverview: function() {
+        //deferred objects
+        var oDefEinkauf = jQuery.Deferred();
+        var oDefZahler = jQuery.Deferred();
+        var oDefGeschaeft = jQuery.Deferred();
 
-			//global vars
-			var oThat = this;
+        //global vars
+        var oThat = this;
 
-			//get firebase id
-            this.getFireBaseIdToken().then(function(token) {
-                console.warn(token);
-            }, function(error) {
-                console.error(error);
+        //get firebase id
+        this.getFireBaseIdToken().then(
+          function(token) {
+            //get purchases
+            var purchase = new Purchase(null, null, null, null, null, token);
+            purchase.getAllPurchases(function(oData, oError) {
+              if (oError === null) {
+                oDefEinkauf.resolve(new JSONModel(oData));
+              } else {
+                MessageToast.show("Die Einkäufe konnten nicht geladen werden.");
+                console.warn(oError);
+                oDefEinkauf.reject();
+              }
             });
+          },
+          function(error) {
+            if (
+              window.confirm(
+                "Wir konnten dich nicht eindeutig identifizieren. Die App wird neu geladen."
+              )
+            ) {
+              location.reload();
+            }
+          }
+        );
 
-			//get purchases
-			var purchase = new Purchase();
-			purchase.getAllPurchases(function(oData, oError) {
-				if (oError === null) {
-					oDefEinkauf.resolve(new JSONModel(oData));
-				} else {
-					MessageToast.show("Die Einkäufe konnten nicht geladen werden.");
-					console.warn(oError);
-					oDefEinkauf.reject();
-				}
-			});
+        //set payer
+        var payer = new Payer();
+        payer
+          .getPayers()
+          .then(
+            function(oData) {
+              oDefZahler.resolve(new JSONModel(oData));
+            },
+            function(error) {
+              MessageToast.show("Die Zahler konnten nicht geladen werden.");
+              console.warn(
+                "Payer entity konnte im promise nicht requestet werden"
+              );
+              oDefZahler.reject();
+            }
+          )
+          .catch(function(err) {
+            console.warn(err);
+          });
 
-			//set payer
-			var payer = new Payer();
-			payer.getPayers().then(
-				function(oData){
-					oDefZahler.resolve(new JSONModel(oData));
-				}, 
-				function(error){
-					MessageToast.show("Die Zahler konnten nicht geladen werden.");
-					console.warn("Payer entity konnte im promise nicht requestet werden");
-					oDefZahler.reject();
-				}
-			).catch(function (err) {
-				  console.warn(err);
-			});
+        //set shops
+        var shop = new Shop();
+        shop
+          .getShops()
+          .then(
+            function(oData) {
+              oDefGeschaeft.resolve(new JSONModel(oData));
+            },
+            function(error) {
+              MessageToast.show("Die Geschäfte konnten nicht geladen werden.");
+              console.warn("Shops Entity konnte nicht aufgerufen werden.");
+              oDefGeschaeft.reject();
+            }
+          )
+          .catch(function(err) {
+            console.warn(err);
+          });
 
-			//set shops
-			var shop = new Shop();
-			shop.getShops().then(
-				function(oData){
-					oDefGeschaeft.resolve(new JSONModel(oData));
-				},
-				function(error){
-					MessageToast.show("Die Geschäfte konnten nicht geladen werden.");
-					console.warn("Shops Entity konnte nicht aufgerufen werden.");
-					oDefGeschaeft.reject();
-				}
-			).catch(function (err) {
-				  console.warn(err);
-			});
+        //check for deferred objects
+        $.when(oDefGeschaeft, oDefZahler, oDefEinkauf)
+          .done(function(oGeschaeft, oZahler, oEinkauf) {
+            //set raw model
+            oThat.getView().setModel(
+              new JSONModel({
+                geschaeft: oGeschaeft.oData.results,
+                zahler: oZahler.oData.results,
+                einkauf: oEinkauf.oData.results
+              }),
+              "raw"
+            );
 
-			//check for deferred objects
-			$.when(oDefGeschaeft, oDefZahler, oDefEinkauf).done(function(oGeschaeft, oZahler, oEinkauf) {
-				//set raw model
-				oThat.getView().setModel(new JSONModel({
-					geschaeft : oGeschaeft.oData.results,
-					zahler : oZahler.oData.results,
-					einkauf : oEinkauf.oData.results
-				}), "raw");
+            //setup month
+            oThat.setUpTableModelByDate();
+          })
+          .fail(function() {
+            MessageToast.show(
+              "Es ist ein Fehler aufgetreten. Probiere es später noch einmal."
+            );
+            console.warn(
+              "request queue konnte nicht erfolgreich abgeschlossen werden"
+            );
+          });
+      },
 
-				//setup month
-				oThat.setUpTableModelByDate();
-			}).fail(function() {
-				MessageToast.show("Es ist ein Fehler aufgetreten. Probiere es später noch einmal.");
-				console.warn("request queue konnte nicht erfolgreich abgeschlossen werden");
-			});
-
-		},
-
-		/**
+      /**
 		 * setup model for table by month
 		 * @memberOf module:Overview
 		 */
-		setUpTableModelByDate : function() {
-			var oEinkaeufe = this.getView().getModel("raw").oData.einkauf;
-			var oZahler = this.getView().getModel("raw").oData.zahler;
-			var oGeschaeft = this.getView().getModel("raw").oData.geschaeft;
-			var oModel2 = new JSONModel();
-			var oTmpModel = {
-				results : []
-			};
-			var dStartDate = this.getView().byId("overviewCalendar").getStartDate();
-			var dEndDate = this.getLastDayOfMonth(dStartDate.getFullYear(), dStartDate.getMonth() + 1);
+      setUpTableModelByDate: function() {
+        var oEinkaeufe = this.getView().getModel("raw").oData.einkauf;
+        var oZahler = this.getView().getModel("raw").oData.zahler;
+        var oGeschaeft = this.getView().getModel("raw").oData.geschaeft;
+        var oModel2 = new JSONModel();
+        var oTmpModel = {
+          results: []
+        };
+        var dStartDate = this.getView().byId("overviewCalendar").getStartDate();
+        var dEndDate = this.getLastDayOfMonth(
+          dStartDate.getFullYear(),
+          dStartDate.getMonth() + 1
+        );
 
-			//set title
-			this.setPageTitle(dStartDate.getMonth());
+        //set title
+        this.setPageTitle(dStartDate.getMonth());
 
-			//build temp einkaeufe model
-			for (var i1 = 0; i1 < oEinkaeufe.length; i1++) {
-				if (new Date(oEinkaeufe[i1].eink_datum) >= dStartDate && new Date(oEinkaeufe[i1].eink_datum) <= dEndDate) {
-					oTmpModel.results.push(oEinkaeufe[i1]);
-				}
-			}
+        //build temp einkaeufe model
+        for (var i1 = 0; i1 < oEinkaeufe.length; i1++) {
+          if (
+            new Date(oEinkaeufe[i1].eink_datum) >= dStartDate &&
+            new Date(oEinkaeufe[i1].eink_datum) <= dEndDate
+          ) {
+            oTmpModel.results.push(oEinkaeufe[i1]);
+          }
+        }
 
-			//add zahler and geschaeft  to temp einkaeufe model
-			for (var i = 0; i < oTmpModel.results.length; i++) {
-				//add zah_alias to oTmpModel
-				for (var j = 0; j < oZahler.length; j++) {
-					if (oTmpModel.results[i].zah_id === oZahler[j].zah_id) {
-						oTmpModel.results[i].zah_alias = oZahler[j].zah_alias;
-					}
-				}
+        //add zahler and geschaeft  to temp einkaeufe model
+        for (var i = 0; i < oTmpModel.results.length; i++) {
+          //add zah_alias to oTmpModel
+          for (var j = 0; j < oZahler.length; j++) {
+            if (oTmpModel.results[i].zah_id === oZahler[j].zah_id) {
+              oTmpModel.results[i].zah_alias = oZahler[j].zah_alias;
+            }
+          }
 
-				//add ges_name to oTmpModel
-				for (var k = 0; k < oGeschaeft.length; k++) {
-					if (oTmpModel.results[i].ges_id === oGeschaeft[k].ges_id) {
-						oTmpModel.results[i].ges_name = oGeschaeft[k].ges_name;
-					}
-				}
-			}
+          //add ges_name to oTmpModel
+          for (var k = 0; k < oGeschaeft.length; k++) {
+            if (oTmpModel.results[i].ges_id === oGeschaeft[k].ges_id) {
+              oTmpModel.results[i].ges_name = oGeschaeft[k].ges_name;
+            }
+          }
+        }
 
-			oModel2.setData(oTmpModel);
-			this.getView().setModel(oModel2, "Einkaeufe");
+        oModel2.setData(oTmpModel);
+        this.getView().setModel(oModel2, "Einkaeufe");
 
-			//setup calendar
-			this.setupCalendarSpecialDates();
+        //setup calendar
+        this.setupCalendarSpecialDates();
 
-			//set footer summary
-			this.setPageFooter();
-		},
+        //set footer summary
+        this.setPageFooter();
+      },
 
-		/**
+      /**
 		 * get last date of month
 		 * @memberOf module:Overview
 		 * @param {string} Year - year as string
 		 * @param {string} Month - month as string
 		 * @return {Date} - returns the date
 		 */
-		getLastDayOfMonth : function(Year, Month) {
-			return new Date((new Date(Year, Month, 1)) - 1);
-		},
+      getLastDayOfMonth: function(Year, Month) {
+        return new Date(new Date(Year, Month, 1) - 1);
+      },
 
-		/**
+      /**
 		  * set page title
 		  * @memberOf module:Overview
 		  * @param {integer} iMonth - month as integer [0...11]
 		  */
-		setPageTitle : function(iMonth) {
-			var month = [];
-			month[0] = "January";
-			month[1] = "February";
-			month[2] = "March";
-			month[3] = "April";
-			month[4] = "May";
-			month[5] = "June";
-			month[6] = "July";
-			month[7] = "August";
-			month[8] = "September";
-			month[9] = "October";
-			month[10] = "November";
-			month[11] = "December";
-			var sMonth = month[iMonth];
+      setPageTitle: function(iMonth) {
+        var month = [];
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+        var sMonth = month[iMonth];
 
-			this.getView().byId("overviewPage").setTitle("Purchases: " + sMonth);
-		},
+        this.getView().byId("overviewPage").setTitle("Purchases: " + sMonth);
+      },
 
-		/**
+      /**
 		 * set view footer with summary
 		 * @memberOf module:Overview
 		 */
-		setPageFooter : function() {
-			var oEinkaeufe = this.getView().getModel("Einkaeufe").oData.results;
-			var oZahlerExt = jQuery.extend(true, [], this.getView().getModel("raw").oData.zahler);
-			var oZah1Count = 0.0;
-			var oZah2Count = 0.0;
+      setPageFooter: function() {
+        var oEinkaeufe = this.getView().getModel("Einkaeufe").oData.results;
+        var oZahlerExt = jQuery.extend(
+          true,
+          [],
+          this.getView().getModel("raw").oData.zahler
+        );
+        var oZah1Count = 0.0;
+        var oZah2Count = 0.0;
 
-			for (var i = 0; i < oEinkaeufe.length; i++) {
-				for (var j = 0; j < this.getView().getModel("raw").oData.zahler.length; j++) {
-					if (oEinkaeufe[i].zah_id === this.getView().getModel("raw").oData.zahler[j].zah_id) {
-						//ext zahler model with count
-						for (var k = 0; k < oZahlerExt.length; k++) {
-							if (oZahlerExt[k].zah_id === oEinkaeufe[i].zah_id) {
+        for (var i = 0; i < oEinkaeufe.length; i++) {
+          for (
+            var j = 0;
+            j < this.getView().getModel("raw").oData.zahler.length;
+            j++
+          ) {
+            if (
+              oEinkaeufe[i].zah_id ===
+              this.getView().getModel("raw").oData.zahler[j].zah_id
+            ) {
+              //ext zahler model with count
+              for (var k = 0; k < oZahlerExt.length; k++) {
+                if (oZahlerExt[k].zah_id === oEinkaeufe[i].zah_id) {
+                  if (oZahlerExt[k].zah_count === undefined) {
+                    oZahlerExt[k].zah_count = 0;
+                  }
 
-								if (oZahlerExt[k].zah_count === undefined) {
-									oZahlerExt[k].zah_count = 0;
-								}
+                  oZahlerExt[k].zah_count =
+                    parseFloat(oZahlerExt[k].zah_count) +
+                    parseFloat(oEinkaeufe[i].eink_wert);
+                }
+              }
+            }
+          }
+        }
 
-								oZahlerExt[k].zah_count = parseFloat(oZahlerExt[k].zah_count) + parseFloat(oEinkaeufe[i].eink_wert);
-							}
-						}
-					}
-				}
-			}
+        try {
+          //try to fix decimals
+          this.getView()
+            .byId("userMani")
+            .setText("Mani: " + oZahlerExt[0].zah_count.toFixed(2) + " €");
+          this.getView()
+            .byId("userNici")
+            .setText("Nici: " + oZahlerExt[1].zah_count.toFixed(2) + " €");
+        } catch (error) {
+          if (error.name === "TypeError") {
+            console.warn("no payments in selected month");
+          } else {
+            throw "uncaught error: in setPageFooter method (overview.controller.js)";
+          }
+        }
+      },
 
-			try {
-				//try to fix decimals
-				this.getView().byId("userMani").setText("Mani: " + oZahlerExt[0].zah_count.toFixed(2) + " €");
-				this.getView().byId("userNici").setText("Nici: " + oZahlerExt[1].zah_count.toFixed(2) + " €");
-			} catch (error) {
-				if (error.name === "TypeError") {
-					console.warn("no payments in selected month");
-				} else {
-					throw "uncaught error: in setPageFooter method (overview.controller.js)";
-				}
-			}
-
-			
-		},
-
-		/**
+      /**
 		 * on change calendar handler; setup table model; close the calendar popover
 		 * @memberOf module:Overview
 		 */
-		onChangeCal : function() {
-			this.setUpTableModelByDate();
-			this.getView().byId("calendarPopover").close();
-		},
+      onChangeCal: function() {
+        this.setUpTableModelByDate();
+        this.getView().byId("calendarPopover").close();
+      },
 
-		/**
+      /**
 		 * set special dates
 		 * @memberOf module:Overview
 		 */
-		setupCalendarSpecialDates : function() {
+      setupCalendarSpecialDates: function() {
+        var oEinkaeufe = this.getView().getModel("raw").oData.einkauf;
+        var oCal = this.getView().byId("overviewCalendar");
+        oCal.destroySpecialDates();
 
-			var oEinkaeufe = this.getView().getModel("raw").oData.einkauf;
-			var oCal = this.getView().byId("overviewCalendar");
-			oCal.destroySpecialDates();
+        for (var i = 0; i < oEinkaeufe.length; i++) {
+          //add special date to cal
+          oCal.addSpecialDate(
+            new DateTypeRange({
+              startDate: new Date(oEinkaeufe[i].eink_datum),
+              type: "Type10"
+            })
+          );
+        }
+      },
 
-			for (var i = 0; i < oEinkaeufe.length; i++) {
-				//add special date to cal
-				oCal.addSpecialDate(new DateTypeRange({
-					startDate : new Date(oEinkaeufe[i].eink_datum),
-					type : 'Type10'
-				}));
-			}
-
-		}, 
-		
-		// set device model
-		setupDeviceModel: function(){
-			var oDeviceModel = new JSONModel(Device);
-			this.getView().setModel(oDeviceModel, "device");
-		},
-		
-		/**
-		 * setup models and set to view
-		 */
-		setupModels: function(){
-			this.setupDeviceModel();
-			this.setupOverview();
-		}
-	});
-});
+      // set device model
+      setupDeviceModel: function() {
+        this.getView().setModel(new JSONModel(sap.ui.Device), "device");
+	  }
+	  
+    });
+  }
+);
--- WebContent/PM/model/Purchase.js
+++ WebContent/PM/model/Purchase.js
@@ -1,101 +1,123 @@
 /*jshint esversion: 6 */
-sap.ui.define([ "sap/ui/base/Object", "sap/ui/model/json/JSONModel" ], function(BaseObject, JSONModel) {
+sap.ui.define(["sap/ui/base/Object", "sap/ui/model/json/JSONModel"], function(
+  BaseObject,
+  JSONModel
+) {
+  var Purchase = BaseObject.extend("mpn/PM/model/Purchase", {
+    constructor: function(
+      purchaseId,
+      purchaseDate,
+      purchaseValue,
+      shopId,
+      payerId,
+      fbIdToken
+    ) {
+      this._purchaseId = purchaseId;
+      this._purchaseDate = purchaseDate;
+      this._purchaseValue = purchaseValue;
+      this._shopId = shopId;
+      this._payerId = payerId;
 
-	var Purchase = BaseObject.extend("mpn/PM/model/Purchase", {
-		constructor : function(purchaseId, purchaseDate, purchaseValue, shopId, payerId, fbIdToken) {
-			this._purchaseId = purchaseId;
-			this._purchaseDate = purchaseDate;
-			this._purchaseValue = purchaseValue;
-			this._shopId = shopId;
-			this._payerId = payerId;
-			
-			this.sHostUrl = 'http://192.168.20.20';
-			this.sHostPort = '3000';
-			this.sConnString = this.sHostUrl + ':' + this.sHostPort;
-			this.sPurchaseEntityUrl = '/EinkaufEntity';
-			this.sPurchaseEntitySetUrl = '/EinkaufEntitySet';
-			this.sExpandByLocation = 'byGeschaeft';
+      this.sHostUrl = "http://localhost";//"http://192.168.20.20";
+      this.sHostPort = "3000";
+      this.sConnString = this.sHostUrl + ":" + this.sHostPort;
+      this.sPurchaseEntityUrl = "/EinkaufEntity";
+      this.sPurchaseEntitySetUrl = "/EinkaufEntitySet";
+      this.sExpandByLocation = "byGeschaeft";
 
-			this.firebaseIdToken = fbIdToken;
-		},
+      this.firebaseIdToken = fbIdToken;
+    },
 
-		/**
+    /**
 		 *  get purchase object as json object
 		 */
-		getPurchaseJson : function() {
-			return {
-				eink_id : this._purchaseId,
-				eink_datum : this._purchaseDate,
-				eink_wert : this._purchaseValue,
-				ges_id : this._shopId,
-				zah_id : this._payerId
-			};
-		},
+    getPurchaseJson: function() {
+      return {
+        eink_id: this._purchaseId,
+        eink_datum: this._purchaseDate,
+        eink_wert: this._purchaseValue,
+        ges_id: this._shopId,
+        zah_id: this._payerId
+      };
+    },
 
-		/**
+    /**
 		  * create a new purchase
 		  * post request
 		  */
-		createPurchase : function(callback) {
-			$.ajax({
-				dataType : "json",
-				contentType : "application/json",
-				method : "POST",
-				url : this.sConnString + this.sPurchaseEntityUrl,
-				data : JSON.stringify(this.getPurchaseJson())
-			})
-				.done(function(data, textStatus, jqXHR) {
-					callback(null, data);
-				})
-				.fail(function(jqXHR, textStatus, errorThrown) {
-					callback(errorThrown, null);
-				});
-		},
+    createPurchase: function(callback) {
+      $.ajax({
+        dataType: "json",
+        contentType: "application/json",
+        method: "POST",
+        url: this.sConnString + this.sPurchaseEntityUrl,
+        data: JSON.stringify(this.getPurchaseJson())
+      })
+        .done(function(data, textStatus, jqXHR) {
+          callback(null, data);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          callback(errorThrown, null);
+        });
+    },
 
-		/**
+    /**
 		 * delete a purchase
 		 * delete request
 		 */
-		deletePurchase : function(sPurchaseId, callback) {
-			$.ajax({
-				method : "DELETE",
-				url : this.sConnString + this.sPurchaseEntityUrl + '/' + sPurchaseId
-			})
-				.done(function(data, textStatus, jqXHR) {
-					callback(null, data);
-				})
-				.fail(function(jqXHR, textStatus, errorThrown) {
-					callback(errorThrown, null);
-				});
-		},
+    deletePurchase: function(sPurchaseId, callback) {
+      $.ajax({
+        method: "DELETE",
+        url: this.sConnString + this.sPurchaseEntityUrl + "/" + sPurchaseId
+      })
+        .done(function(data, textStatus, jqXHR) {
+          callback(null, data);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          callback(errorThrown, null);
+        });
+    },
 
-		/**
+    /**
 		 * get purchases
 		 * get request
 		 */
-		getAllPurchases : function(callback) {
-			$.ajax(this.sConnString + this.sPurchaseEntitySetUrl)
-				.done(function(data, textStatus, jqXHR) {
-					callback(data, null);
-				})
-				.fail(function(jqXHR, textStatus, errorThrown) {
-					callback(null, errorThrown);
-				});
-		}, 
-		
-		/**
+    getAllPurchases: function(callback) {
+      $.ajax({
+        method: "GET",
+        url: this.sConnString + this.sPurchaseEntitySetUrl,
+        headers: {
+          Authorization: this.firebaseIdToken
+        }
+      })
+        .done(function(data, textStatus, jqXHR) {
+          callback(data, null);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          callback(null, errorThrown);
+        });
+    },
+
+    /**
 		 * get purchases by ges_id
 		 */
-		getPurchasesByShopId: function(iGesId, callback){
-			$.ajax(this.sConnString + this.sPurchaseEntitySetUrl + '/' + this.sExpandByLocation + '/' + iGesId)
-			.done(function(data, textStatus, jqXHR) {
-				callback(data, undefined);
-			})
-			.fail(function(jqXHR, textStatus, errorThrown) {
-				callback(undefined, errorThrown);
-			});
-		}
-	});
+    getPurchasesByShopId: function(iGesId, callback) {
+      $.ajax(
+        this.sConnString +
+          this.sPurchaseEntitySetUrl +
+          "/" +
+          this.sExpandByLocation +
+          "/" +
+          iGesId
+      )
+        .done(function(data, textStatus, jqXHR) {
+          callback(data, undefined);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          callback(undefined, errorThrown);
+        });
+    }
+  });
 
-	return Purchase;
+  return Purchase;
 });
